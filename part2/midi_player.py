38ad7952aa1e00a05101f71e815fdd56adffe1b2
Randall Bohn (dexter) <rsbohn@gmail.com>
2021-07-10 05:37:32 -0600

    track current note, send NoteOff when appropriate

--- part2/midi_player.py
+++ part2/midi_player.py
@@ -9,6 +9,7 @@ import usb_midi
 # use `circup install adafruit_midi`
 import adafruit_midi
 from adafruit_midi.note_on import NoteOn
+from adafruit_midi.note_off import NoteOff
 
 
 class MidiPlayer:
@@ -21,12 +22,13 @@ class MidiPlayer:
             in_channel=1,
             out_channel=1,
         )
-        self.bpm = 10
+        self.bpm = 20
         self.tick_time = 60 / self.bpm * 1 / 4
         self.velocity = 120
         self.last_tick = 0
         self.step = 0
         self.playing = False
+        self.current_note = None
         # do re mi...
         self.riff = [60, 62, 64, 65, 67, 69, 71, 72]
 
@@ -35,7 +37,8 @@ class MidiPlayer:
         self.playing = True
         self.step = 0
         self.last_tick = time.monotonic()
-        self.midi.send(NoteOn(self.riff[0], self.velocity))
+        self.current_note = self.riff[0]
+        self.midi.send(NoteOn(self.current_note, self.velocity))
         return self
 
     def stop(self):
@@ -51,9 +54,12 @@ class MidiPlayer:
 
     def play(self, now):
         """Play the next note after self.tick_time has elapsed."""
-        if self.tick_time <= now - self.last_tick:
+        if self.playing and self.tick_time <= now - self.last_tick:
             self.last_tick = now
             self.step += 1
-            note = self.riff[self.step % len(self.riff)]
-            self.midi.send(NoteOn(note, self.velocity))
+            self.current_note = self.riff[self.step % len(self.riff)]
+            self.midi.send(NoteOn(self.current_note, self.velocity))
+        if not self.playing and self.current_note is not None:
+            self.midi.send(NoteOff(self.current_note, 0))
+            self.current_note = None
         return self.step
